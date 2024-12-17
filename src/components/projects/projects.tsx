9f7072ee0d0184c0d5d9a5f9bd55cf2272e61c8b
Lukáš Rác <rac@uxf.cz>
2024-12-17 23:09:14 +0100

    projects page - empty state

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -1,11 +1,15 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
+import { useTranslations } from "next-intl";
 import { PROJECTS } from "@/config/constants";
 import { Technology } from "@/config/types";
+import { isEmpty } from "@/utils/is-empty";
 import { ProjectTile } from "../project-tile/project-tile";
 
 export function Projects() {
+    const t = useTranslations();
+
     const searchParams = useSearchParams();
 
     const selectedLanguages = searchParams.get("language")?.split(",") ?? [];
@@ -15,6 +19,10 @@ export function Projects() {
             project.technologies.includes(lang as Technology),
         ),
     );
+
+    if (isEmpty(filteredProjects)) {
+        return <p>{t("ProjectsPage.empty-projects")}</p>;
+    }
 
     return (
         <div className="grid auto-rows-fr gap-10 lg:grid-cols-2 2xl:grid-cols-3">
