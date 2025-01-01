321776ecb65c8064d87ac5a66e88b6fa8de9a8f3
Lukáš Rác <rac@uxf.cz>
2025-01-01 23:32:37 +0100

    email fix

--- src/utils/send-email.tsx
+++ src/utils/send-email.tsx
@@ -5,6 +5,9 @@ import InquiryEmail from "../../emails/inquiry";
 
 const transporter = nodemailer.createTransport({
     service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // true for 465, false for other ports
     auth: {
         user: process.env.SMTP_SERVER_USERNAME,
         pass: process.env.SMTP_SERVER_PASSWORD,
