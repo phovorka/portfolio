1cd35fef44b25fe547021a6f92b12b1435d77efc
hovorkapetr <hovorkapetr@googlemail.com>
2025-09-12 13:37:00 +0000

    new env data

--- src/utils/send-email.tsx
+++ src/utils/send-email.tsx
@@ -2,7 +2,7 @@ import { render } from "@react-email/components";
 import nodemailer from "nodemailer";
 import { ContactFormData } from "@/components/contact-form/contact-form";
 import InquiryEmail from "../../emails/inquiry";
-
+//new env
 const transporter = nodemailer.createTransport({
     host: "smtp.mailersend.net",
     port: 587,
